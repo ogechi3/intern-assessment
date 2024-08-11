9a0eb66b49c71d9748143b576b7777b3fd86691d
Thomas Jennifer <jsamuelt13@gmail.com>
2024-08-11 01:07:33 +0100

    add navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { FaFacebookF,FaTwitter,FaInstagram } from 'react-icons/fa';
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <h1>Foodieland.</h1>
+        <ul>
+          <li>Home</li>
+          <li>Recipe</li>
+          <li>Blog</li>
+          <li>Contact</li>
+          <li>About Us</li>
+        </ul>
+        <div className="social-icons">
+          <FaFacebookF className="icons" />
+          <FaTwitter className="icons" />
+          <FaInstagram className="icons" />
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar
